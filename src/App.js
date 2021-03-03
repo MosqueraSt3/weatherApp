1d7242f431c3f6dcf4621bc529f387894b7d4ec9
MosqueraSt3 <esmosquera66@misena.edu.co>
2021-03-03 15:51:27 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react'
+
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+import Clima from './components/Clima'
+import Error from './components/Error'
 
 function App() {
+
+  // STATE
+  const [ busqueda, guardarBusqueda ] = useState({
+    ciudad: '',
+    pais: ''
+  })
+  const [ consultar, guardarConsultar ] = useState(false)
+  const [ resultado, guardarResultado ] = useState({})
+  const [ error, guardarError ] = useState(false)
+
+  const { ciudad, pais } = busqueda
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appID = 'e94cfacb28e1029d1290e25ebef9959c'
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        guardarResultado(resultado)
+        guardarConsultar(false)
+
+        
+      if (resultado.cod === '404') {
+        guardarError(true)
+      } else {
+        guardarError(false)
+      }
+      }
+    }
+    consultarAPI()
+    // eslint-disable-next-line
+  }, [consultar])
+
+  let componente
+  if (error) {
+    componente = <Error 
+      mensaje="No hay Resultado"
+    />
+  } else {
+    componente = <Clima 
+      resultado={resultado}
+    />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+      <Header 
+        titulo='Weather App'
+      />
+
+      <div
+        className="contenedor-form"
+      >
+        <div
+          className="container"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div 
+            className="row"
+          >
+            <div
+              className="col m6 s12"
+            >
+              <Formulario 
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
+            </div>
+            <div
+              className="col m6 s12"
+            >
+              {componente}
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </Fragment>
+  )
 }
 
 export default App;
